6ff76328d3a37176fce600be83ef0d45c6c69aba
Celestino <kcelestinomaria127@gmail.com>
2021-06-12 11:50:59 +0300

    committed scss code

--- src/components/sections/FeaturesSplit.js
+++ src/components/sections/FeaturesSplit.js
@@ -67,13 +67,13 @@ const FeaturesSplit = ({
             <div className="split-item">
               <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
-                  Lightning fast workflow
+                  Robust workflow
                   </div>
                 <h3 className="mt-0 mb-12">
-                  Safe and Secured Transactions
+                  Fast, Straightforward &amp; Standardized workflow
                   </h3>
                 <p className="m-0">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  Set up your business profile, document your business exit plan, upload the required documents, and list your business, let Exitor deal with the rest. 
                   </p>
               </div>
               <div className={
