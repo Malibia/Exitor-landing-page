99b3f7bba42a4e0664d878996dddb61f2e9f774e
Celestine Kariuki <46893114+kcelestinomaria@users.noreply.github.com>
2021-06-12 15:10:41 +0300

    Update ModalDialog.js

--- src/components/ModalDialog.js
+++ src/components/ModalDialog.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Dialog from '@material-ui/core/Dialog';
-import Form from 'elements/Form';
+import Form from './elements/Form';
 
 const ModalDialog = ({ open, handleClose }) => {
   return (
-    // props received from App.js
+    {/*// props received from App.js*/}
     <Dialog open={open} onClose={handleClose}>
       // form to be created
       <Form handleClose={handleClose} />
